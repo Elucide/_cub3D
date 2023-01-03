6c641022473c3fec985eb5a6c58aeb4f0af09a60
Yoseph SCHECROUN <yschecro@e2r1p2.clusters.42paris.fr>
2023-01-03 23:40:59 +0100

    useless printf

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,7 +6,7 @@
 /*   By: yschecro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 23:30:49 by yschecro          #+#    #+#             */
-/*   Updated: 2023/01/03 23:35:21 by yschecro         ###   ########.fr       */
+/*   Updated: 2023/01/03 23:38:54 by yschecro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	key_hook(int keycode, void *param)
 {
+	dprintf(2, "keycode is %d\n", keycode);
 	if (keycode == 65307)
 		close_win(0);
 	return ((void) param, 1);
