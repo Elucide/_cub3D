fb47d79efdf9cb03e360ebe7bf35ec99eee992f6
Romain BENAYOUN-WEIL <rbenayou@e2r2p3.clusters.42paris.fr>
2023-01-04 21:52:50 +0100

    norme parsing

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: rbenayou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 19:00:10 by rbenayou          #+#    #+#             */
-/*   Updated: 2023/01/04 21:35:48 by rbenayou         ###   ########.fr       */
+/*   Updated: 2023/01/04 21:51:20 by rbenayou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,38 @@
 
 int	is_space(char c)
 {
-	if (c == '\t' || c == '\v' || c == '\n' || c == '\r' || c == '\f' || c == ' ') 
+	if (c == '\t' || c == '\v' || c == '\n' || c == '\r'
+		|| c == '\f' || c == ' ')
 		return (1);
 	return (0);
 }
 
 int	check_extension(char *path)
 {
-	if (!ft_strcmp(ft_strnstr(path, ".cub", ft_strlen(path)), ".cub"))	
-		return(1);
-	return(0);
+	if (!ft_strcmp(ft_strnstr(path, ".cub", ft_strlen(path)), ".cub"))
+		return (1);
+	return (0);
 }
 
 char	*check_element(char *s)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 	char	*tmp;
 
 	i = 0;
 	j = 0;
-	while(s[i] && is_space(s[i]))
+	while (s[i] && is_space(s[i]))
 		i++;
 	if (!s[i])
-		return(NULL);
-	while(s[i] && !is_space(s[i]))
+		return (NULL);
+	while (s[i] && !is_space(s[i]))
 	{
 		j++;
 		i++;
 	}
 	tmp = ft_substr(s, i - j, j);
-	while(s[i] && is_space(s[i]))
+	while (s[i] && is_space(s[i]))
 	{
 		if (s[i + 1] && is_space(s[i]) && !is_space(s[i + 1]))
 		{
@@ -53,14 +54,14 @@ char	*check_element(char *s)
 		}
 		i++;
 	}
-	return(tmp);
+	return (tmp);
 }
 
 char	*store_element(char *element, char *s)
 {
 	t_data	*data;
 
-	data=_data();
+	data = _data();
 	if (element != NULL)
 	{
 		printf("Error\nDouble declaration\n");
@@ -68,7 +69,7 @@ char	*store_element(char *element, char *s)
 	}
 	s += 2;
 	data->nb_el++;
-	return(check_element(s));
+	return (check_element(s));
 }
 
 void	check_digit(char **tab)
@@ -77,17 +78,17 @@ void	check_digit(char **tab)
 	int	j;
 
 	i = 0;
-	while(tab[i])
+	while (tab[i])
 	{
 		j = 0;
-		while(tab[i][j])
+		while (tab[i][j])
 		{
 			if (!ft_isdigit(tab[i][j]))
 			{
 				printf("Error\nInvalid declaration\n");
 				free_garbage();
 			}
-			j++;		
+			j++;
 		}
 		i++;
 	}
@@ -95,8 +96,8 @@ void	check_digit(char **tab)
 
 int	parse_color(char *s)
 {
-	int	coma;
-	int	i;
+	int		coma;
+	int		i;
 	char	**tmp;
 
 	i = 0;
@@ -112,15 +113,16 @@ int	parse_color(char *s)
 		tmp = ft_split(s, ',');
 		i = 0;
 		check_digit(tmp);
-		while(tmp[i])
+		while (tmp[i])
 		{
-			if(ft_atoi(tmp[i]) >= 0 && ft_atoi(tmp[i]) <= 255)
+			if (ft_atoi(tmp[i]) >= 0 && ft_atoi(tmp[i]) <= 255)
 				i++;
 			else
-				break;
+				break ;
 		}
 		if (i == 3)
-			return(rgb_convert(ft_atoi(tmp[0]),ft_atoi(tmp[1]),ft_atoi(tmp[2])));
+			return (rgb_convert(ft_atoi(tmp[0]), ft_atoi(tmp[1])
+					, ft_atoi(tmp[2])));
 	}
 	printf("Error\nInvalid declaration\n");
 	free_garbage();
@@ -131,7 +133,7 @@ int	store_color(int element, char *s)
 {
 	t_data	*data;
 
-	data=_data();
+	data = _data();
 	if (element > 0)
 	{
 		printf("Error\nDouble declaration\n");
@@ -140,18 +142,18 @@ int	store_color(int element, char *s)
 	s += 1;
 	s = check_element(s);
 	data->nb_el++;
-	return(parse_color(s));
+	return (parse_color(s));
 }
 
 int	check_line(char *tmp)
 {
 	t_data	*data;
 
-	data=_data();
+	data = _data();
 	while (tmp[0] && is_space(tmp[0]))
 		tmp++;
 	if (!tmp[0])
-		return(1);
+		return (1);
 	if (!ft_strncmp(tmp, "NO", 2) && is_space(tmp[2]))
 		data->no = store_element(data->no, tmp);
 	else if (!ft_strncmp(tmp, "SO", 2) && is_space(tmp[2]))
@@ -164,15 +166,15 @@ int	check_line(char *tmp)
 		data->floor = store_color(data->floor, tmp);
 	else if (!ft_strncmp(tmp, "C", 1) && is_space(tmp[1]))
 		data->ceiling = store_color(data->ceiling, tmp);
-	else if (tmp[0] == '1' || tmp[0] == '0' || tmp[0] == 'N' || tmp[0] == 'S' || tmp[0] == 'E' || tmp[0] == 'W')
-		return(0);
+	else if (tmp[0] == '1' || tmp[0] == '0' || tmp[0] == 'N'
+		|| tmp[0] == 'S' || tmp[0] == 'E' || tmp[0] == 'W')
+		return (0);
 	else
 	{
 		printf("Error\nInvalid declaration\n");
 		free_garbage();
 	}
-	return(1);
-
+	return (1);
 }
 
 void	parse_elements(void)
@@ -180,15 +182,15 @@ void	parse_elements(void)
 	t_data	*data;
 	char	*tmp;
 
-	data=_data();
+	data = _data();
 	data->nb_el = 0;
-	while(1)
+	while (1)
 	{
 		tmp = get_next_line(data->fd);
-		if(tmp == NULL)
-			break;
+		if (tmp == NULL)
+			break ;
 		if (!check_line(tmp))
-			break;
+			break ;
 	}
 	if (data->nb_el != 6)
 	{
@@ -197,11 +199,11 @@ void	parse_elements(void)
 	}
 }
 
-void 	parse(void)
+void	parse(void)
 {
-	t_data *data;
+	t_data	*data;
 
-	data=_data();
+	data = _data();
 	data->path = check_element(data->path);
 	if (data->path != NULL && check_extension(data->path))
 	{
@@ -209,16 +211,17 @@ void 	parse(void)
 		if (data->fd > 0)
 		{
 			parse_elements();
-			printf("NO =%s\n",data->no);
-			printf("SO =%s\n",data->so);
-			printf("WE =%s\n",data->we);
-			printf("EA =%s\n",data->ea);
-			printf("floor =%d\n",data->floor);
-			printf("ceiling =%d\n",data->ceiling);
 			//parse_map();
-			return;
+			return ;
 		}
 	}
 	printf("Error\nInvalid path\n");
 	exit(EXIT_FAILURE);
 }
+
+/*printf("NO =%s\n",data->no);
+  printf("SO =%s\n",data->so);
+  printf("WE =%s\n",data->we);
+  printf("EA =%s\n",data->ea);
+  printf("floor =%d\n",data->floor);
+  printf("ceiling =%d\n",data->ceiling);*/
